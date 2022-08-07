74ab1cd097653975bf027f059654f4a26fadfacc
shashankrana5 <rana_s1@denison.edu>
2022-08-07 13:45:05 -0400

    first commit

---
+++ src/main/java/com/sudoku/sudokuAssembly/SudokuAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudokuAssembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuAssemblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuAssemblyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudoku/sudokuAssembly/SudokuAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudokuAssembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuAssemblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
