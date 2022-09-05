62e8caf5709b84a54e938312d95a559de59c8273
shashankrana5 <shashankrana316@gmail.com>
2022-09-05 00:09:00 -0400

    added the wrong checker functionality where it displays a red border around the <td>. I need to remove it once the user gets it correct

--- src/main/resources/templates/static/index.js
+++ src/main/resources/templates/static/index.js
@@ -1,11 +1,5 @@
-// var testingValue;
-// for (var i = 0; i< 81; i++){
-//     testingValue = ""+(i/2);
-//     console.log(typeof testingValue);
-    
-// }
-
 puzzle = "8,0,0,1,0,7,6,0,0,0,6,2,0,0,8,4,0,7,7,0,1,0,6,2,8,0,0,0,3,0,0,8,6,0,4,2,0,2,6,0,0,0,0,0,5,9,1,0,4,0,0,0,3,0,0,5,0,8,3,0,0,7,0,1,0,9,0,5,0,0,0,8,0,0,3,6,0,0,5,9,0";
+solution = "8,9,5,1,4,7,6,2,3,3,6,2,5,9,8,4,1,7,7,4,1,3,6,2,8,5,9,5,3,7,9,8,6,1,4,2,4,2,6,7,1,3,9,8,5,9,1,8,4,2,5,7,3,6,6,5,4,8,3,9,2,7,1,1,7,9,2,5,4,3,6,8,2,8,3,6,7,1,5,9,4";
 
 
 console.log(puzzle.length)
@@ -13,36 +7,27 @@ for (var i = 0; i< puzzle.length; i+=2){
 
     if (puzzle[i] != '0') document.getElementById("sudoku-cell-"+(i/2)).innerText = puzzle[i];
 
-    else document.getElementById("sudoku-cell-" + (i/2)).appendChild(createInput());
+    else document.getElementById("sudoku-cell-" + (i/2)).appendChild(createInput(i/2));
 }
 
-function createInput(){
+function createInput(index){
     var newInput = document.createElement("input");
     newInput.setAttribute("type", "number");
     newInput.setAttribute("min", "1");
     newInput.setAttribute("max", "9");
-    newInput.classList.add("cell");
+    newInput.classList.add("cell-" + index);
     return newInput;
 }
-
-// const allValues = document.querySelector(".making");
-
-// const allValues = document.getElementsByClassName("testing-cell");
-// console.log(allValues);
-// Array.from(allValues).forEach(element => {
-//     console.log(element);
-//     // element.addEventListner("click", (e)=>
-//     // console.log("HIT!"))
-// })
-
 
 let elementsArray = document.querySelectorAll("input");
 
 elementsArray.forEach(function(elem) {
-    elem.addEventListener("click", (e) =>{
-    // console.log("HI")
-    if (e.target.value != "4"){
-        elem.style.border = "solid red";
+    elem.addEventListener("input", (e) =>{
+        console.log(elem.className);
+        console.log("sudoku-cell-"+elem.className.substring(5));
+        console.log(parseInt(solution[parseInt(elem.className.substring(5))*2]));
+    if (e.target.value != parseInt(solution[parseInt(elem.className.substring(5))*2])){ // The value here has to be an integer.
+        document.querySelector("#sudoku-cell-"+elem.className.substring(5)).style.border = "solid red";
     }
 });
 });
