a91c97051f665d295b014423675e434fd7cc62f2
shashankrana5 <shashankrana316@gmail.com>
2022-09-09 23:46:43 -0400

    made it display the solutions for the sudoku

--- src/main/resources/templates/static/index.js
+++ src/main/resources/templates/static/index.js
@@ -24,9 +24,20 @@ let elementsArray = document.querySelectorAll("input");
 elementsArray.forEach(function(elem) {
     elem.addEventListener("input", (e) =>{
     if (e.target.value != parseInt(solution[parseInt(elem.className.substring(5))*2])){ // The value here has to be an integer.
-        document.querySelector("#sudoku-cell-"+elem.className.substring(5)).style.border = "solid red";
-    }
+        var sudokuCellId = "#sudoku-cell-"+elem.className.substring(5);
+        var tempCell = document.querySelector(sudokuCellId);
+        if ([2, 5, 11].includes(parseInt(sudokuCellId.substring(13)))){
+            tempCell.style.border = "solid red";
+            tempCell.style.borderRight = "4px solid red";
+        }
+        else{
+            tempCell.style.border = "solid red";
+
+        }
+
+        }   
     else{
+        //Changes the box shape after getting the correct value: needs to change the borders of 2, 5, 11 and so on.
         document.querySelector("#sudoku-cell-"+ elem.className.substring(5)).style.border = "solid black";
     }
 });
@@ -36,71 +47,10 @@ var showSolutionButton = document.querySelector("#solve-button");
 
 showSolutionButton.addEventListener("click", (e)=>{
     document.querySelector(".cell-2").value = 3;
+    for (var i = 0; i< solution.length; i+=2){
+        if (puzzle[i] == "0")
+        document.querySelector(".cell-" + (i/2)).value = solution[i];
+    }
 }
     
 );
-
-
-
-
-
-
-// Array.from(allValues).forEach(element => {
-//     console.log(element);
-//   });
-
-// allValues.forEach(console.log("hi"));
-
-
-
-
-
-// const cellInput = document.querySelector(".cell");
-
-// cellInput.addEventListener("click", thisfuc);
-
-// function thisfuc(e){
-//     console.log(e.target.value)
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// cellInput.addEventListener("input", inputValidator);
-
-// function inputValidator(e){
-
-//     if (e.target.value != "4"){
-//         cellInput.style.border= "red";
-//     }
-//     console.log(e.target.value);
-// }
-
-
-// const inp = document.querySelector("input");
-
-// inp.addEventListener("input", thisFunc);
-
-// function thisFunc(e){
-//     console.log(e.target.value)
-// }
-
-
-
-
-
-// var testingValue = document.querySelector("#testing-right");
-// console.log(testingValue.value);
-// testingValue.appendChild(document.createElement("input"));
-
-// var anotherTesting = document.getElementById("another-testing");
-// anotherTesting.innerText = "This is the printeirn ";
