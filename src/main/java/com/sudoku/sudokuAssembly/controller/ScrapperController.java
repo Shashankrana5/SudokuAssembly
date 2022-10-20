1b92459bb2dd9cc2263276511eff2c1a1f7e86fd
shashankrana5 <shashankrana316@gmail.com>
2022-10-20 11:32:21 -0400

    added frontend file from react

--- src/main/java/com/sudoku/sudokuAssembly/controller/ScrapperController.java
+++ src/main/java/com/sudoku/sudokuAssembly/controller/ScrapperController.java
@@ -1,24 +1,37 @@
 package com.sudoku.sudokuAssembly.controller;
 
+import com.sudoku.sudokuAssembly.entity.Sudoku;
 import com.sudoku.sudokuAssembly.service.ScrapperService;
+import com.sudoku.sudokuAssembly.service.SudokuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @RestController
+@CrossOrigin("*")
 public class ScrapperController {
 
     private final ScrapperService scrapperService;
+    private final SudokuService sudokuService;
+
 
     @Autowired
-    public ScrapperController(ScrapperService scrapperService) {
+    public ScrapperController(ScrapperService scrapperService, SudokuService sudokuService) {
         this.scrapperService = scrapperService;
+        this.sudokuService = sudokuService;
     }
 
     @GetMapping("/scrape")
     public void scrape() throws IOException {
         scrapperService.scrape();
+    }
+
+    @GetMapping("/reactingtesting")
+    public ArrayList<Sudoku> reactingtesting(){
+        return sudokuService.findAllSudoku();
     }
 }
