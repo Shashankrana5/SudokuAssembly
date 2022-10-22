7fe9df14b9ba55aeb9d6ef60bcf88cbc4e21069d
shashankrana5 <shashankrana316@gmail.com>
2022-10-22 14:08:02 -0400

    got spring security setup properly to work with the login authentication, however, I will have to make it now connect with the data from the database

--- src/main/java/com/sudoku/sudokuAssembly/entity/User.java
+++ src/main/java/com/sudoku/sudokuAssembly/entity/User.java
@@ -15,8 +15,11 @@ public class User {
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
-
     private String lastName;
+
+    @Column(name ="username")
+    private String username;
+
     @Column(name = "email")
     private String email;
 
--- src/main/java/com/sudoku/sudokuAssembly/security/SecurityConfiguration.java
+++ src/main/java/com/sudoku/sudokuAssembly/security/SecurityConfiguration.java
@@ -1,13 +1,17 @@
 package com.sudoku.sudokuAssembly.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,17 +19,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user").password("password").roles("USER").build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin").password("password").roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(user,admin);
-    }
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user").password("password").roles("USER").build();
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin").password("password").roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(user,admin);
+//    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -40,5 +44,21 @@ public class SecurityConfiguration {
                 .httpBasic()
                 .and().build();
     }
+
+    @Autowired
+    UserDetailsService userDetailService;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+
+
 
 }
