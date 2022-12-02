65c0fd39f6cb5d621342b0618baa7c76be52d990
shashankrana5 <shashankrana316@gmail.com>
2022-12-02 09:57:45 -0500

    made changes to the scrapper service so that it will get the data from the raw data instead of a sub section of it

--- src/main/java/com/sudoku/sudokuAssembly/service/impl/ScrapperServiceImpl.java
+++ src/main/java/com/sudoku/sudokuAssembly/service/impl/ScrapperServiceImpl.java
@@ -41,16 +41,19 @@ public class ScrapperServiceImpl implements ScrapperService {
         List<String> puzzleList = new ArrayList<>();
 
 
-        String scrapeData = elements.get(2).toString();
+        String scrapeData = elements.toString();
         Pattern p = Pattern.compile("[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+");
         Matcher m = p.matcher(scrapeData);
         while (m.find()) {
             puzzleList.add(m.group());
         }
-        int date_index = elements.get(2).toString().indexOf("print_date");
-        String date = elements.get(2).toString().substring(date_index + 13, date_index + 23);
+        int date_index = elements.toString().indexOf("print_date");
+        String date = elements.toString().substring(date_index + 13, date_index + 23);
+//        System.out.println(puzzleList.get(0) + "     " +  puzzleList.get(1));
+//        System.out.println(puzzleList.get(2) + "     " +  puzzleList.get(3));
+//        System.out.println(puzzleList.get(4) + "     " +  puzzleList.get(5));
 
-        //Adding sudokus:
+//        Adding sudokus:
         sudokuRepository.save(new Sudoku(UUID.randomUUID(), date + "-NewYorkTimes", puzzleList.get(0), "easy", "New York", date, puzzleList.get(1)));
         sudokuRepository.save(new Sudoku(UUID.randomUUID(), date + "-NewYorkTimes", puzzleList.get(4), "medium", "New York", date, puzzleList.get(5)));
         sudokuRepository.save(new Sudoku(UUID.randomUUID(), date + "-NewYorkTimes", puzzleList.get(2), "hard", "New York", date, puzzleList.get(3)));
