05e99cb511ec46480f85e8c511e648c27d6f1eda
shashankrana5 <shashankrana316@gmail.com>
2024-01-13 21:24:41 -0500

    Updated the scraper endpoint and update docker file to show mention the volume that is going to be used.

--- server/src/main/java/com/sudoku/sudokuAssembly/controller/ScrapperController.java
+++ server/src/main/java/com/sudoku/sudokuAssembly/controller/ScrapperController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/scrape")
 public class ScrapperController {
 
     private final ScrapperService scrapperService;
